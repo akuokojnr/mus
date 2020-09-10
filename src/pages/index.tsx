a7ab913fa58f45acfeaad5e71d2fc15d3ba0a619
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2020-09-10 13:48:53 +0000

    fix: remove unnecessary files

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "components/Layout";
+import SEO from "components/SEO";
+
+const IndexPage: React.FC = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Homepage</h1>
+  </Layout>
+);
+
+export default IndexPage;
